8d1919c53ba9c283a9037e90ac6e6465179ddc52
Mads Nedergaard <nedergaardmads@gmail.com>
2018-03-02 13:31:23 +0100

    Created utils file with colors

---
+++ app/utils.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const colors = {
+    background: '#232855',
+    primary: '#5FCC9C',
+    primaryDark: '#215B63',
+    primaryLight: '#AAFFC7',
+    text: '#ffffff'
+}
