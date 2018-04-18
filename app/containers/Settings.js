bc49730c5e4765fb11d0bcd7431a1ed2d98be6ae
Mads Nedergaard <nedergaardmads@gmail.com>
2018-04-18 08:02:04 +0200

    Removed hardcoded credentials and added input fields

--- app/containers/Settings.js
+++ app/containers/Settings.js
@@ -7,29 +7,26 @@ import * as firebase from 'firebase';
 import Title from '../components/Title';
 import { colors } from '../utils';
 
-// Insert hardcoded credentials
-// TODO: Find better way to handle authentication
-const EMAIL = ''; 
-const PASSWORD = '';
-
-
 class Settings extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            loggedIn: false
+            loggedIn: false,
+            email: '',
+            password: ''
         }
     }
     
-async signup(email, pass) {
-    try {
-        await firebase.auth().createUserWithEmailAndPassword(email, pass);
+async signup(email, password) {
+    try { 
+        await firebase.auth().createUserWithEmailAndPassword(email, password);
         console.log('Account created');
         alert('Sådan, du er nu oprettet');
         this.setState({loggedIn: true});
         // Navigate to the Home page, the user is auto logged in
     } catch (error) {
         console.log(error.toString());
+        alert(error.toString());
     }
 }
 async login(email, pass) {
@@ -41,6 +38,7 @@ async login(email, pass) {
         // Navigate to the Home page
     } catch (error) {
         console.log(error.toString());
+        alert(error.toString());
     }
 }
 
@@ -53,10 +51,26 @@ async login(email, pass) {
                     :
                     <View>
                         <Title h4>Du er ikke logget ind: </Title>
-                        <Button buttonStyle={{ margin: 10, backgroundColor: colors.primary, borderRadius: 30}} title={'Signup'} onPress={() => this.signup(EMAIL, PASSWORD)} />
-                    
-                      
-                        <Button buttonStyle={{ margin: 10, backgroundColor: colors.primary, borderRadius: 30}} title={'Login'} onPress={() => this.login(EMAIL, PASSWORD)} />
+                        <View style={styles.signupContainer}>
+                            <Input
+                            placeholder='Email'
+                            inputStyle={styles.input}
+                            inputContainerStyle={styles.inputContainer}
+                            onChangeText={(text) => this.setState({email: text})}
+                            value={this.state.email}
+                            />
+                            <Input
+                            placeholder='Password'
+                            inputStyle={styles.input}
+                            secureTextEntry={true}
+                            inputContainerStyle={styles.inputContainer}
+                            onChangeText={(text) => this.setState({password: text})}
+                            value={this.state.password}
+                            />
+                        </View>
+                        <Button buttonStyle={{ margin: 10, backgroundColor: colors.primary, borderRadius: 30}} title={'Login'} onPress={() => this.login(this.state.email, this.state.password)} />
+                        <Title>ELLER</Title>
+                        <Button buttonStyle={{ margin: 10, backgroundColor: colors.primary, borderRadius: 30}} title={'Signup'} onPress={() => this.signup(this.state.email, this.state.password)} />
                     </View>
                 }
             </View>
@@ -70,6 +84,20 @@ const styles = StyleSheet.create({
         padding: 20,
         paddingTop: 0,
         alignSelf: 'stretch'
+    },
+    signupContainer: {
+        alignItems: 'center',
+
+    },
+    inputContainer: {
+        borderRadius: 30,
+        marginTop: 10,
+        marginLeft: 20,
+        marginRight: 20,
+        paddingLeft: 10,
+        backgroundColor: colors.text
+},
+    input: {
     },
     map: {
         flex: 1,
